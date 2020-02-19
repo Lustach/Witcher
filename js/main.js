97de0fc41a653546220bde2dbbb8d9cce470091e
Lustach <lusta.vlad2001@gmail.com>
2020-02-19 13:03:29 +0300

    UPD

--- js/main.js
+++ js/main.js
@@ -6,13 +6,45 @@ var mySwiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.arrow',
       },
-
 });
-console.log('gav');
+// console.log(window.screen.width);
+// if(window.screen.width<=375){
+//     mySwiper.params.slidesPerView=1
+//     let classes= document.querySelectorAll('.series-2')
+//     console.log(classes);
+//     for (let index = 0; index < classes.length; index++) {
+//         console.log(classes[index].style.display);
+//         classes[index].style.display='none'
+//     }
+// }
 let btn = document.querySelector('.button')
 // console.log(btn);
 btn.addEventListener('click',(e)=>{
     console.log(e)
 })
 // console.log(btn);
-console.log(mySwiper);
+// console.log(mySwiper);
+
+let body = document.querySelector('body')
+// window.screen.addEventListener('change',(e)=>{
+//     console.log(screen.width);
+// })
+// window.addEventListener('resize',function(){
+//     // alert('Размеры окна изменились');
+//     switch (window.screen.width) {
+//         case 375:
+//             console.log(375);
+//             mySwiper.params.slidesPerView=1
+//             console.log(mySwiper.params.slidesPerView);
+//             break;
+//         case 425:
+//             console.log(425);
+//         default:
+//             break;
+//     }
+//     // console.log(window.screen.width);
+// });
+
+// let w = window.screen.availWidth;
+// console.log(w);
+// console.log(screen.width);
